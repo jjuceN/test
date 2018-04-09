a9888e44d1c29ff234ce93c9f67751c3915f1c35
jjuceN <2315958728@qq.com>
2018-04-09 20:00:35 +0800

    sss

---
+++ mylibrary/src/androidTest/java/com/example/ye/mylibrary/Test.java
@@ -0,0 +1,15 @@
+package com.example.ye.mylibrary;
+
+/**
+ * Created by fangtao on 2018-04-09.
+ */
+
+public class Test {
+
+    public double addition(double addnum1,double addnum2){
+
+        return addnum1+ addnum2;
+
+    }
+
+}
